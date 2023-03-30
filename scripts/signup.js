7980c91264dcb5d58fcc827f94ebafb84b0fe7d0
Cauê Souza <caue.souzapl@gmail.com>
2023-03-29 22:57:54 -0300

    feat: signup completed

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,5 +1,7 @@
 const formElement = document.getElementById('sign-up')
 const inputsElement = document.querySelectorAll("input");
+const responseElement = document.getElementById('response')
+const API = 'https://todo-api.ctd.academy/v1/users'
 
 function validateForm() {
     let formIsValid = true;
@@ -23,9 +25,62 @@ function validateForm() {
     return formIsValid;
 }
 
-function handleForm(e) {
+function catchData() {
+    const data = {
+        firstName: formElement['name'].value,
+        lastName: formElement['last-name'].value,
+        email: formElement['email'].value,
+        password: formElement['password'].value
+    }
+
+    return data
+}
+
+function sendResponse(response) {
+    if (response === 200) {
+        responseElement.innerHTML = `<span class="ok">usuario criado com sucesso!</span>`
+    } else if (response === 400) {
+        responseElement.innerHTML = `<span class="err">email ja cadastrado!</span>`
+    } else {
+        responseElement.innerHTML = `<span class="err">erro no servidor</span>`
+    }
+}
+
+function sendData(e) {
     e.preventDefault();
 
+    const config = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(catchData())
+    }
+
+    fetch(API, config)
+        .then(response => {
+            if (response.ok) {
+                response.json().then( token => {
+                    sessionStorage.setItem('jwt', JSON.stringify(token.jwt))
+                    setTimeout( () => {
+                        window.location.href = './tarefas.html';
+                    }, 5000)
+                })
+                sendResponse(200)
+                formElement.reset();
+            }
+            else if (response.status >= 400 && response.status < 403) {
+                sendResponse(400)
+            }
+            else {
+                sendResponse()
+            }
+        })
+
+}
+
+function handleForm() {
     let formIsValid = validateForm();
 
     if (!formIsValid) {
@@ -37,3 +92,4 @@ function handleForm(e) {
 }
 
 formElement.addEventListener('keyup', handleForm);
+formElement.addEventListener('submit', (e) => sendData(e))
